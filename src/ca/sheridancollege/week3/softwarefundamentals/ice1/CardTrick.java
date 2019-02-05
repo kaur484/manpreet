7e31e12e313f573ce64a25c0a070b5c13a001958
Manpreet Kaur <mkkhokhar96@gmail.com>
2019-02-05 17:57:44 -0500

    Test Class

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
@@ -10,23 +11,67 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modified by MANPREET KAUR MANPREET KAUR
+STUDENT NUMBER: 991342529
  */
+import java.util.Scanner;
+
 public class CardTrick {
     
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
+        
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            magicHand[i] = new Card();
+            int v = (int) (Math.random() * 13 + 1);
+            magicHand[i].setValue(v);
+            int s = (int)(Math.random()*3);
+            magicHand[i].setSuit(Card.SUITS[s]);
+           
+            System.out.println(magicHand[i].getSuit()+"  "+ magicHand[i].getValue());
         }
+        Scanner obj = new Scanner (System.in);//scanner object
+        Card eight = new Card();//object for the eight class
+       
+        System.out.println("Enter the value for card (1-13):");
+        int userChoice = obj.nextInt();
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        
+        System.out.println("Enter the suit: 0 for Hearts, 1 for Diamonds, 2 for Spades and 3 for  Clubs");
+        int userSuits = obj.nextInt();
+        boolean flag =false;
+        
+        switch(userSuits){
+            case 0: 
+               eight.setSuit(Card.SUITS[0]);
+            break;
+            case 1: 
+               eight.setSuit(Card.SUITS[1]);
+            break;
+            case 2: 
+               eight.setSuit(Card.SUITS[2]);
+            break;
+            case 3: 
+               eight.setSuit(Card.SUITS[3]);
+            break;         
+        }
+        eight.setValue(userChoice);
+         //comparing the values if present in array
+         
+         for(int i =0; i <magicHand.length; i++){
+             if(eight.getValue()== magicHand[i].getValue()  && eight.getSuit()== magicHand[i].getSuit()){                
+                 System.out.println("CARD FOUND");
+                 
+               flag = true;
+         }
+         
     }
+         if(flag = false){
+                 
+             System.out.println("CARD NOT FOUND");       
+             }     
     
+}
 }
